b00a4777de4aa56f8ae73ed0d5dbb8c8a4c6944a
Babanazar Gutlygeldiyev <g.babanazar@gmail.com>
2024-03-24 12:18:59 +0000

    primitives done

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -5,13 +5,15 @@
 // Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    let (first, second) = a.split_at(1);
+    let (k, l) = second.split_at(3);
+
+    let nice_slice = k;
 
     assert_eq!([2, 3, 4], nice_slice)
 }
